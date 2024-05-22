facda99237f5a00e32c1771f6dd217d55c9a464f
halaaniba <156901595+halaaniba@users.noreply.github.com>
2024-05-22 14:24:21 +0200

    deploy

--- src/main/java/com/example/ese16/NameController.java
+++ src/main/java/com/example/ese16/NameController.java
@@ -1,12 +1,15 @@
 package com.example.ese16;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/devname")
+
 public class NameController {
-    @GetMapping("/devName")
-    public String devName(){
-        return ("hello");
+    @GetMapping
+    public String devName() {
+        return "hala";
     }
 }
